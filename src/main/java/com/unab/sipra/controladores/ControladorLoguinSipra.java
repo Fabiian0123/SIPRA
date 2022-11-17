e0c69a17f8872caad3edb8934ead1ac669629fed
Fabiian0123 <est_gf_santanderf333@fesc.edu.co>
2022-11-17 10:49:00 -0500

    se aplican cambios al momento de ingresar, y se implementa el gitignore

--- src/main/java/com/unab/sipra/controladores/ControladorLoguinSipra.java
+++ src/main/java/com/unab/sipra/controladores/ControladorLoguinSipra.java
@@ -1,6 +1,7 @@
 package com.unab.sipra.controladores;
 
 import com.unab.sipra.LoguinGui.LoguinSipra;
+import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
 
 
@@ -15,18 +16,43 @@ public class ControladorLoguinSipra {
         ventanaUno.setVisible(false);
     }
     
-    public static void btnEmpezar(){
+    public static void btnEmpezar(JCheckBox checkEstudiante, JCheckBox checkDocente, JCheckBox checkExtension){
         String Usuario, Contraseña;
         Usuario= ventanaUno.getCajaUsuarioLoguinSipraGui().getText();
         Contraseña=ventanaUno.getCajaContraseñaLoguinSipraGui().getText();
         if(Usuario.equals("admin") && Contraseña.equals("admin")){
-            hidden();
-            ControladorInterfazDocente.inicio();
-           
+            
+            if (checkEstudiante.isSelected()){
+            
+            } else if (checkDocente.isSelected()) {
+                hidden();
+                ControladorInterfazDocente.inicio();
+            } else if (checkExtension.isSelected()) { 
+            
+            } else {
+                JOptionPane.showMessageDialog(ventanaUno,"POR FAVOR SELECCIONE UN ROL");
+            }
         }else{
             JOptionPane.showMessageDialog(ventanaUno,"CONTRASEÑA O USUARIO INCORRECTA");
         }
     }
    
+    public static void onCheckRoles(JCheckBox checkEstudiante, JCheckBox checkDocente, JCheckBox checkExtension) {
+        int rol = (checkEstudiante.isSelected()) ? 1 :
+                (checkDocente.isSelected()) ? 2 :
+                (checkExtension.isSelected()) ? 3 : 0;
+        
+        checkEstudiante.setSelected(false);
+        checkDocente.setSelected(false);
+        checkExtension.setSelected(false);
+                
+        if (rol == 1)
+            checkEstudiante.setSelected(true);        
+        if (rol == 2)
+            checkDocente.setSelected(true);        
+        if (rol == 3)
+            checkExtension.setSelected(true);
+        
+    }
     
 }
